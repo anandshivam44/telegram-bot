30b9eca3b7819d24b02e6014b55a5d7b2fd945c8
anandshivam44 <anand.shivam44@yahoo.com>
2020-12-05 16:45:22 +0530

    Added heroku cred

--- echobot.py
+++ echobot.py
@@ -59,7 +59,7 @@ def main():
     updater.start_webhook(listen="0.0.0.0",
                           port=int(PORT),
                           url_path=TOKEN)
-    updater.bot.setWebhook('https://yourherokuappname.herokuapp.com/' + TOKEN)
+    updater.bot.setWebhook('https://obscure-savannah-01072.herokuapp.com/' + TOKEN)
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
