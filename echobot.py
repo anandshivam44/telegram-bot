3b7f204e47a248ca2c0e8d830ea123e3f7bdd963
anandshivam44 <anand.shivam44@yahoo.com>
2020-12-06 20:51:43 +0530

    added scraping

--- echobot.py
+++ echobot.py
@@ -1,9 +1,11 @@
 import logging
-
+import requests
+from bs4 import BeautifulSoup
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 import os
 PORT = int(os.environ.get('PORT', 5000))
+url = 'http://bitsindri.ac.in/'
 
 # Enable logging
 logging.basicConfig(
@@ -28,11 +30,23 @@ def help_command(update: Update, context: CallbackContext) -> None:
 
 def echo(update: Update, context: CallbackContext) -> None:
     """Echo the user message."""
-    print("\n\n")
-    print(update)
-    print("\n\n")
-    print(context)
-    update.message.reply_text(update.message.text)
+    # print("\n\n")
+    # print(update)
+    # print("\n\n")
+    # print(context)
+    reply = None
+    if update.message.text is not None:
+        reply = ""
+        r = requests.get(url)
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36(KHTML, like Gecko) Chrome/41.0.2272.0 Safari/537.36'}
+        r = requests.get(url, headers=headers)
+        htmlContent = r.content
+        soup = BeautifulSoup(htmlContent, 'html.parser')
+        list_ul = soup.find_all('ul', class_="nav menu menu-treemenu")
+        for i in range(1, 11):
+            reply += list_ul[0].contents[i].find('span').getText()+"\n\n"
+        update.message.reply_text(reply)
 
 
 def main():
